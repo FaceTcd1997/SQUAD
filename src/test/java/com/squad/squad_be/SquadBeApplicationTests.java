e457dbc60b5a04e256e225dd4ef48bb071a94b5a
FaceTcd1997 <117104430+FaceTcd1997@users.noreply.github.com>
2023-03-14 15:45:08 +0000

    Initial commit

---
+++ src/test/java/com/squad/squad_be/SquadBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad.squad_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
