859d8c3f36287db63cd6a1f756ebde918c0675c0
FaceTcd1997 <117104430+FaceTcd1997@users.noreply.github.com>
2023-03-14 16:24:21 +0000

    Hello World get microservice

--- src/main/java/com/squad/squad_be/SquadBeApplication.java
+++ src/main/java/com/squad/squad_be/SquadBeApplication.java
@@ -2,12 +2,20 @@ package com.squad.squad_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SquadBeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SquadBeApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
     }
 
 }
